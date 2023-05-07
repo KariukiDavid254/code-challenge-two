a0afd72ffe9e0f04472ee73f89fcfff27f4c3fc0
KariukiDavid254 <kariukidavidwangari254@gmail.com>
2023-05-07 12:43:55 +0300

    rendered bots

--- bot-battlr/src/App.js
+++ bot-battlr/src/App.js
@@ -1,40 +1,12 @@
-import React, { useState } from 'react';
-import BotCollection from './components/BotCollection';
-import YourBotArmy from './components/YourBotArmy';
 import './App.css';
+import YourBotArmy from './YourBotArmy';
 
-const App = () => {
-const [army, setArmy] = useState([]);
-
-const handleBotEnlisted = (bot) => {
-if (!army.find(b => b.id === bot.id)) {
-setArmy([...army, bot]);
+function App() {
+  return (
+    <div className="App">
+      <YourBotArmy />
+    </div>
+  );
 }
-};
-
-const handleBotRelease = (id) => {
-setArmy(army.filter(b => b.id !== id));
-};
-
-const handleBotDischarge = (id) => {
-fetch(`https://my-json-server.typicode.com/KariukiDavid254/bots/bots/${id}`, {
-method: 'DELETE',
-})
-.then(response => {
-if (response.ok) {
-setArmy(army.filter(b => b.id !== id)); 
-}
-})
-.catch(error => console.error(error));
-};
-
-return (
-  <div>
-<YourBotArmy army={army} releaseBot={handleBotRelease} dischargeBot={handleBotDischarge} setArmy={setArmy} />
-
-<BotCollection onBotEnlisted={handleBotEnlisted} onBotRelease={handleBotRelease} />
-</div>
-);
-};
 
 export default App;
